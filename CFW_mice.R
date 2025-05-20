fb33c0a52d109b85ba6d9a6633fa88cd1f4590ae
Jason-Teng <faf046059@gmail.com>
2025-05-19 23:15:41 -0700

    Desktop

--- CFW_mice.R
+++ CFW_mice.R
@@ -2,34 +2,45 @@ pheno <- read.csv("pheno.csv")
 geno <- read.csv(gzfile("geno.txt.gz"), sep=" ")
 map <- read.csv("map.txt", sep=" ")
 
-
+### phenotype selection
 pheno_var <- pheno[(pheno$discard=="no"),c("id","TA", "EDL","bw1", "taillength", "tibia", "testisweight")]
 pheno_var_noNa <- na.omit(pheno_var)
+
+p = ncol(pheno_var_noNa)-1 # number of traits
 
 # plot the variables
 library("PerformanceAnalytics")
 chart.Correlation(pheno_var_noNa[,2:ncol(pheno_var_noNa)], histogram=TRUE, pch=19)
 
 
-G = geno[(geno$discard=="no"),3:ncol(geno)]
-P_var = pheno_var_noNa[,c("id", "BMD", "TA", "EDL", "taillength", "p120b1")]#,"PPIweight"
+### genotype 
+geno_nodiscard = geno[(geno$discard=="no"),]
 
-P_nodiscard_noNa <- na.omit(P_var)
+k = ncol(geno_nodiscard) -2# number of mar
 
-PandG <- merge(P_nodiscard_noNa,geno, by = "id") # should combine in the end
+PandG <- merge(geno_nodiscard,pheno_var_noNa, by = "id") # inner join (only include common row)
 
-P <- scale(PandG[,2:6])
-G <- PandG[,8:ncol(PandG)]
+P <- scale(PandG[,(3+k):ncol(PandG)])
+write.table(P, "cfw_gemma_pheno.txt",sep="\t", col.names = FALSE, row.names = FALSE)
+write.csv(P, "cfw_pheno.csv", row.names = FALSE)
+
+G <- PandG[,3:(2+k)]
+G_gemma <- cbind(map[, c("id","alt","ref")], t(G))
+write.table(G_gemma, "cfw_gemma_geno.txt", sep=",",col.names = FALSE, row.names = FALSE)
+
+map_gemma<- map[,c("id","pos","chr")]
+write.table(map_gemma, "cfw_gemma_annot.txt",sep="\t", col.names = FALSE, row.names = FALSE)
+
 G <- round(G)-1 # the original datast is 0, 1,2
 
 maf_values <- apply(G, 2, calculate_maf) 
 
 which(is.na(maf_values)) # no NA
-snps_index <- which(maf_values > 0.01) 
+snps_index <- which(maf_values > 0.05) 
 cfw_gen_filter <- G[,snps_index]
 cfw_map_filter <- map[snps_index, ]
 write.csv(cfw_gen_filter, "cfw_gen_maffilter.csv",row.names = FALSE)
-write.csv(ara_map_filter, "cfw_map_maffilter.csv",row.names = FALSE)
+write.csv(cfw_map_filter, "cfw_map_maffilter.csv",row.names = FALSE)
 
 
 cfw_map_filter <- read.csv("cfw_map_maffilter.csv")
@@ -45,6 +56,8 @@ write.csv(K, "K_cfw.csv", row.names = FALSE)
 
 ## profiling gwas
 cfw_gen_filter <- read.csv("cfw_gen_maffilter.csv")
+P <- read.csv("cfw_pheno.csv")
+
 Z = as.matrix(cfw_gen_filter)
 K = as.matrix(read.csv("K_cfw.csv"))
 source("C:/Users/faf04/Dropbox/jason/Abroad/UCR/Research in UCR/Dr. Xu/project/transformed_method_BLUP/R_function/profiling_gwas.R")
@@ -52,6 +65,19 @@ source("C:/Users/jason/Dropbox/jason/Abroad/UCR/Research in UCR/Dr. Xu/project/t
 ID = PandG[,1]
 result_cfw <- profiling_gwas(as.matrix(P), t(Z), K, ID, "CFW")
 
+# manhatton plot
+library(qqman)
+gwasinfo <- data.frame(
+  SNP = cfw_map_filter$id,
+  CHR =  cfw_map_filter$chr,
+  BP = cfw_map_filter$pos,
+  P =result_cfw$log10p
+)
+max_p = max(result_cfw$log10p)
 
-
-## other eda
+genomewideline =-log10(0.05/k)
+manhattan(gwasinfo,col = c("blue4","cyan3"),main="CFW",ylim=c(0,max_p+2),cex=2,
+          logp =FALSE,
+          genomewideline=genomewideline ,#50.17457,
+          ylab="-log10p",
+          suggestiveline=F)
